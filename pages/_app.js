03adb7faf50b0180ebd424e6c050a19d52e0bc53
Yong Jin <70016523+gamcho3@users.noreply.github.com>
2022-01-23 20:41:02 +0900

    frist

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+//import { useEffect, useState } from "react";
+import { Layout } from "../components"; //index값으로 이동
+import "../styles/globals.scss";
+import "tailwindcss/tailwind.css";
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "../components";
+const posts = [
+  { title: "react testing", excerpt: "learn learn testing" },
+  { title: "react tailwind", excerpt: "learn react tailwind" },
+];
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      {/* tailwind css의 문법 */}
+      <Head>
+        <title>CMS blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relatve top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
